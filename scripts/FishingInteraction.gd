fb657b08dcebff2fb105acc1a21163fb0d226569
Erlend Tregde <erlendt@uia.no>
2025-02-22 23:57:20 +0100

    now able to change to the fishing scene

--- scripts/FishingInteraction.gd
+++ scripts/FishingInteraction.gd
@@ -1,20 +1,22 @@
 extends Area2D
 
-@export var fishing_scene_path: String = "res://scenes/fishing_scene"
+var entered = false  
 
-@onready var label = $Label  # Reference the Label node
+@onready var label = $Label 
 
 func _ready():
 	label.visible = false  # Hide the label initially
 
 func _on_body_entered(body):
-	if body.is_in_group("Player"):  # Detect player using group
-		label.visible = true   # Show interaction text
+	if body.is_in_group("Player"):  
+		entered = true
+		label.visible = true  # Show the label when the Player enters
 
 func _on_body_exited(body):
 	if body.is_in_group("Player"):
-		label.visible = false  # Hide text when the player leaves
+		entered = false
+		label.visible = false  # Hide the label when the Player leaves
 
-func _input(event):
-	if event.is_action_pressed("interact") and label.visible:
-		get_tree().change_scene_to_file(fishing_scene_path)  # Switch to FishingScene
+func _process(_delta):
+	if entered and Input.is_action_just_pressed("interact"):
+		get_tree().change_scene_to_file("res://scenes/fishing_scene.tscn")
