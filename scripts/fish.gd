82e988b0fe744df57a0f0f09d704e65a44bd6034
Erlend Tregde <erlendt@uia.no>
2026-01-07 17:23:48 +0100

    deploy website

--- scripts/fish.gd
+++
@@ -1,7 +0,0 @@
-extends Node2D
-
-@onready var area = $Area2D  
-
-func _ready():
-	add_to_group("fish")  
-	print("🐟 Fish initialized:", name)
--- scripts/stop_fishing_button.gd
+++
@@ -1,9 +0,0 @@
-extends Button
-
-func _ready():
-	# Connect the button to the function
-	self.pressed.connect(_on_stop_fishing_pressed)
-
-func _on_stop_fishing_pressed():
-	# Change the scene back to main
-	get_tree().change_scene_to_file("res://scenes/main.tscn")
